2e1a20b8659d2231d96cdda0d051ad140b173970
isaiasrdgjr <isaias.rdg@gmail.com>
2025-05-08 20:12:59 -0300

    Atualizar aula 8.

--- aula08/exemplo3.py
+++ aula08/exemplo3.py
@@ -11,9 +11,22 @@ def cadasrar_pessoa(num):
         lista_vendedor_vendas.append(pessoa)
 
 
+def calcula_total_media():
+    tot = 0
+    for pessoa in lista_vendedor_vendas:
+        tot += pessoa['Valor']
+    med = tot/len(lista_vendedor_vendas)
+
+    return tot, med
+
+
 lista_vendedor_vendas = []
 
 qtd = int(input('Quantas pessoas? '))
 cadasrar_pessoa(qtd)
 
 print(lista_vendedor_vendas)
+
+total, media = calcula_total_media()
+
+print(f'Valores Total de vendas R${total:.2f} e média de vendas R${media:.2f}')
