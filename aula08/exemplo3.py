001ed85a3367fc120e9e1f952b3ac854f7b552fd
isaiasrdgjr <isaias.rdg@gmail.com>
2025-05-08 21:29:59 -0300

    Atualizar exemplos aula 8.

--- aula08/exemplo3.py
+++ aula08/exemplo3.py
@@ -20,6 +20,26 @@ def calcula_total_media():
     return tot, med
 
 
+def calcula_maior_venda():
+    mv = 0
+    v = ''
+    for i in lista_vendedor_vendas:
+        if i['Valor'] > mv:
+            mv = i['Valor']
+            v = i['Nome']
+    return mv, v
+
+
+def busca_vendedor(nm_vend):
+    nv = ''
+    for nm in lista_vendedor_vendas:
+        if nm['Nome'] == nm_vend:
+            nv = nm_vend
+            break
+    return nv
+
+
+#   Def1    #
 lista_vendedor_vendas = []
 
 qtd = int(input('Quantas pessoas? '))
@@ -27,6 +47,21 @@ cadasrar_pessoa(qtd)
 
 print(lista_vendedor_vendas)
 
+#   Def2    #
 total, media = calcula_total_media()
 
 print(f'Valores Total de vendas R${total:.2f} e média de vendas R${media:.2f}')
+
+#   Def3    #
+maior_venda, maior_vendedor = calcula_maior_venda()
+
+print(f'Maior valor de venda R${maior_venda}, vendedor {maior_vendedor}')
+
+#   Def4    #
+nm_vend = input('Busque o vendedor: ')
+nome_vendedor = busca_vendedor(nm_vend)
+
+if nome_vendedor:
+    print(f'O vendedor é: {nome_vendedor}')
+else:
+    print('Vendedor não encontrado.')
